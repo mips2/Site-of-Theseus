6f4a898a733a9fd7d57c1eb1239bfef504fc726b
mips2 <daisymeow999@gmail.com>
2025-01-09 03:55:59 +0000

    new system prompt

--- website/app.py
+++ website/app.py
@@ -6,25 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/explore')
-def explore():
-    return render_template('explore.html')
-
-@app.route('/space')
-def space():
-    return render_template('space.html')
-
-@app.route('/ocean')
-def ocean():
-    return render_template('ocean.html')
-
-@app.route('/forest')
-def forest():
-    return render_template('forest.html')
-
-@app.route('/interactive-galaxy')
-def interactive_galaxy():
-    return render_template('interactive_galaxy.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
